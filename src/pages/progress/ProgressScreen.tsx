05f419c77aa25b6ade7dff48313a657d15ee9c7d
popbojan <popbojan@yahoo.com>
2026-08-24 16:24:05 +0200

    werfe 43% von den Header weg

--- src/pages/progress/ProgressScreen.tsx
+++ src/pages/progress/ProgressScreen.tsx
@@ -126,7 +126,6 @@ export default function ProgressScreen() {
               <MasteryProfileHero
                 progress={displayProgress}
                 levels={masteryLevels}
-                showProgressPercent
               />
             )}
           </View>

39423a6dd2b46fb21404d8316e6edad5069fd399
popbojan <popbojan@yahoo.com>
2026-08-25 12:00:29 +0200

    Change applicationId to: app.mytaskmanager

--- android/app/src/main/java/app/mytaskmanager/MainActivity.kt
+++ android/app/src/main/java/app/mytaskmanager/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.mytaskmanagermobile
+package app.mytaskmanager
 
 import com.facebook.react.ReactActivity
 import com.facebook.react.ReactActivityDelegate
--- android/app/src/main/java/app/mytaskmanager/MainApplication.kt
+++ android/app/src/main/java/app/mytaskmanager/MainApplication.kt
@@ -1,4 +1,4 @@
-package com.mytaskmanagermobile
+package app.mytaskmanager
 
 import android.app.Application
 import com.facebook.react.PackageList
